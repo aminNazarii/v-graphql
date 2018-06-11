1e05e094746f78752d0753d0735aa58bafcc5b93
Amin <aminnazari55@gmail.com>
2018-06-11 10:49:56 +0430

    Init project

---
+++ src/app.js
@@ -0,0 +1,74 @@
+import axios from 'axios';
+
+export default {
+  resources: {},
+  prefix: '/graphql',
+  axios,
+
+  /**
+   *
+   * @param Vue
+   * @param options
+   */
+  install(Vue, options) {
+    Vue.prototype.$graphql = this;
+  },
+
+  /**
+   *
+   * @param resources
+   */
+  setResources(resources) {
+    this.recources = resources;
+  },
+
+  /**
+   *
+   * @param prefix
+   */
+  setPrefix(prefix) {
+    this.prefix = prefix;
+  },
+
+  /**
+   *
+   * @param name
+   * @param config
+   * @returns {AxiosPromise<any>}
+   */
+  query(name, config = {variables: null, schema: ""}) {
+    let resource = this.recources.default.query[name];
+    let schema = '';
+    if (config.schema) {
+      schema = '/' + config.schema;
+    }
+
+    return this.axios.post(
+      this.prefix + schema,
+      {
+        query: resource.replace(/\n|\t/g, ' '),
+        variables: config.variables
+      });
+  },
+
+  /**
+   *
+   * @param name
+   * @param config
+   * @returns {AxiosPromise<any>}
+   */
+  mutation(name, config = {variables: null, schema: ""}) {
+    let resource = this.recources.default.mutation[name];
+    let schema = '';
+    if (config.schema) {
+      schema = '/' + config.schema;
+    }
+
+    return this.axios.post(
+      this.prefix + schema,
+      {
+        query: resource.replace(/\s+/g, ' '),
+        variables: config.variables
+      });
+  },
+}
